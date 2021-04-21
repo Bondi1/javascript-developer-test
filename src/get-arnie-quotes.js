f997ac9763dcbd187e18284e56f2eb9901d7a7ac
Bondi1 <divya.avanna@gmail.com>
2021-04-21 12:11:37 +1000

    implementation of get-arnie-quotes

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,8 +1,38 @@
 const { httpGet } = require('./mock-http-interface');
 
+/**
+ * 
+ * @param {string[]} urls an array of strings, with each string containing an url
+ * @returns {Promise} A promise which resolves to a results array.
+ */
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+   // Create promises array for every url passed
+   //using a map to process them in parallel 
+   const promises = urls.map(async(url) => await httpGet(url));
+
+   // return a promise that resolves to the overall results array
+   return Promise.all(promises).then((responseData) => {
+
+    // results placeholder array
+    let results = [];
+
+    // Traverse through each of the resolved response and populate the results array
+    responseData.forEach(element => 
+    {  
+      //extract the message by parsing the body
+      let json = JSON.parse(element.body);
+
+      // Based on the status code populate different result key-values
+      if(element.status === 200){
+        results.push({'Arnie Quote': json.message});
+      }else{
+        results.push({'FAILURE': json.message});
+      }
+   });
+
+   // return the results array
+   return results;
+  })
 };
 
 module.exports = {
